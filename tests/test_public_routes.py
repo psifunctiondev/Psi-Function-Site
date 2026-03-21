f69a40bb429178ea1d8dfa35bfe40a5b32e98979
Psi Function Development <dev@psifunction.com>
2026-03-21 17:49:39 -0400

    First feature change

--- tests/test_public_routes.py
+++ tests/test_public_routes.py
@@ -5,3 +5,13 @@ def test_home(client):
 def test_about(client):
     response = client.get("/about")
     assert response.status_code == 200
+
+def test_homepage_includes_css(client):
+    resp = client.get("/")
+    html = resp.data.decode()
+
+    assert "site.css" in html
+
+def test_homepage_renders(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
