68f2d75df41fe7e342145a3e04ae3497ee663aa0
Psi Function Development <dev@psifunction.com>
2026-03-30 17:48:33 -0400

    Progress on Home page

--- app/blueprints/public/routes.py
+++ app/blueprints/public/routes.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 public_bp = Blueprint('public', __name__)
 
 @public_bp.get('/')
+def index():
+    return redirect(url_for('public.home'))
+
+@public_bp.get('/home')
 def home():
     return render_template('public/home.html')
 
